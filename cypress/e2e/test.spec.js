34ebc85d21aada29a5c9d2116309eb423d9a1d9b
jean <leejeanhwa@gmail.com>
2022-06-29 14:58:12 +0800

    test

--- cypress/e2e/test.spec.js
+++ cypress/e2e/test.spec.js
@@ -8,7 +8,7 @@ describe('Test Suite', () => {
             failOnStatusCode: false
         })
         cy.get("a[data-title='View More']").should('not.be.empty')
-        // cy.get('#txtSearchInput').type(`${data.wallet}{enter}`)
+        cy.get('#txtSearchInput').type(`${data.wallet}`)
         // cy.get('p.mr-2 > a').invoke('text').then(Number).should('be.gt', 2)
     })
     it('API Test', () => {
